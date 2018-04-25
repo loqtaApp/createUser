790f283651c80b8fc65b6bf21c273f406ecaf285
Mona subaih <m.subaih@rozn.org>
2018-04-25 12:14:46 +0300

    sxcsc

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+echo  phpversion();
+die();
 header('Content-Type: application/json');
 error_reporting(E_ALL);
 //initialize request  to create order with wehook
