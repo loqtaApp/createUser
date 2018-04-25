cddee7e740072830cb8e40259c9e938bc2de3226
Mona subaih <m.subaih@rozn.org>
2018-04-25 12:17:14 +0300

    phph

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-echo  phpversion();
-die();
+
 header('Content-Type: application/json');
 error_reporting(E_ALL);
 //initialize request  to create order with wehook
