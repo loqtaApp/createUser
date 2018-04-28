67388c2a433e63f6d335a7fa24d6ee695dea3433
Mona subaih <m.subaih@rozn.org>
2018-04-28 09:23:52 +0300

    ss

--- index.php
+++ index.php
@@ -13,7 +13,8 @@ use Kreait\Firebase\Factory;
 use Kreait\Firebase\ServiceAccount;
 use Kreait\Auth;
 $data = json_decode(file_get_contents('php://input'),true);
-
+var_dump($data);
+die();
 
 $mainURL = "https://f3aa0d6659405ab34f9c0af85d0f2ef9:590b142f0e9922bd187703cd6729bae8@loqta-ps.myshopify.com/admin/customers/".$data['id']."/metafields.json";
 
